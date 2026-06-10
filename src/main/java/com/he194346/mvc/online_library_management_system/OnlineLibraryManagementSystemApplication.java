6a978b1d31709f12019132811b1d431a50283d33
VuietKie <vuhiep27082005@gmail.com>
2026-06-10 15:36:53 +0700

    Create project

---
+++ src/main/java/com/he194346/mvc/online_library_management_system/OnlineLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.he194346.mvc.online_library_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLibraryManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineLibraryManagementSystemApplication.class, args);
+    }
+
+}
