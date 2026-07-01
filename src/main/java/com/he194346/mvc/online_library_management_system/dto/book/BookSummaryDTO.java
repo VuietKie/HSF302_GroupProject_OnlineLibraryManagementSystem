8eb7f41eaa83a628d365120b9f824486b90066c2
VuietKie <vuhiep27082005@gmail.com>
2026-07-01 20:47:34 +0700

    View Book for ROLE_READER

---
+++ src/main/java/com/he194346/mvc/online_library_management_system/dto/book/BookSummaryDTO.java
@@ -0,0 +1,11 @@
+package com.he194346.mvc.online_library_management_system.dto.book;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookSummaryDTO {
+    private Long bookId;
+    private String title;
+}
--- src/main/java/com/he194346/mvc/online_library_management_system/repository/BookRepository.java
+++ src/main/java/com/he194346/mvc/online_library_management_system/repository/BookRepository.java
@@ -1,7 +1,21 @@
 package com.he194346.mvc.online_library_management_system.repository;
 
 import com.he194346.mvc.online_library_management_system.entity.Book;
+import com.he194346.mvc.online_library_management_system.enums.BookStatus;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
+    boolean existsByTitleIgnoreCase(String title);
+
+    List<Book> findAllByStatusOrderByTitleAsc(BookStatus status);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select b from Book b where b.bookId in :ids order by b.bookId")
+    List<Book> findAllByIdForUpdate(@Param("ids") List<Long> ids);
 }
